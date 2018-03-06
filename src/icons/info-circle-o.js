cd48b7550960a1dbdaacd6617f36ffb899b87086
eyunhua <eyunhua@baidu.com>
2018-03-06 20:48:18 +0800

    新增Alert组件样式，并更新alert组件用到的四种图标

--- src/icons/info-circle-o.js
+++ src/icons/info-circle-o.js
@@ -1,11 +1,12 @@
 import Icon from 'veui/components/Icon'
 Icon.register({
   "info-circle-o": {
-    "width": "16",
-    "height": "16",
+    "width": "12",
+    "height": "12",
     "paths": [
       {
-        "d": "M8 1a7 7 0 1 0 0 14A7 7 0 0 0 8 1zm0-1a8 8 0 1 1 0 16A8 8 0 0 1 8 0zM7 6.87v5h2v-5H7zm1-1a1 1 0 1 0 0-2 1 1 0 0 0 0 2z"
+        "d": "M6 0a6 6 0 1 0 0 12A6 6 0 0 0 6 0zm.78 8.6a.78.78 0 0 1-1.56 0V5.49a.78.78 0 1 1 1.56 0V8.6zM6 4.18a.78.78 0 1 1 0-1.56.78.78 0 0 1 0 1.56z",
+        "fill-rule": "evenodd"
       }
     ]
   }
