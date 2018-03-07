c848ab8eadf5fb4d5310eaa12336df8ebd9b7443
eyunhua <eyunhua@baidu.com>
2018-03-07 15:11:28 +0800

    新增dropdown组件样式

--- demo/components/Dropdown.vue
+++ demo/components/Dropdown.vue
@@ -1,49 +1,75 @@
 <template>
   <article>
     <h1><code>&lt;veui-dropdown&gt;</code></h1>
-    <p>
-      <veui-dropdown
-        ui="primary"
+    <section>
+      <p>
+        <span class="veui-font-level-1b">默认按钮样式：</span>
+        <span class="veui-font-level-2d">类型ui=""（不传）</span>
+      </p>
+      <p>
+        <span class="veui-font-level-2d">大样式：ui="large"</span>
+      </p>
+      <p>
+        <veui-dropdown
+        ui="large"
         label="操作操作操作操作操作操作"
         :options="options"></veui-dropdown>
-      &nbsp;&nbsp;&nbsp;&nbsp;
-      <veui-dropdown
-        ui="primary micro"
+      </p>
+      <p>
+        <span class="veui-font-level-2d">中样式：ui=""（不传）</span>
+      </p>
+      <p>
+        <veui-dropdown
         label="操作操作操作操作操作操作"
-        :options="options">
-        <template slot="option-label" slot-scope="{ label }">
-          👉 {{ label }}
-        </template>
-      </veui-dropdown>
-    </p>
-    <p>
-      <veui-dropdown
-        ui="primary"
-        label="操作操作操作操作操作操作"
-        disabled
         :options="options"></veui-dropdown>
-    </p>
-    <p>
-      <veui-dropdown
-        ui="link"
-        label="操作">
-        <veui-option-group label="提交">
-          <veui-option @click="log('save')" label="保存"/>
-          <veui-option value="publish" label="发布"/>
-        </veui-option-group>
-        <veui-option-group label="操作">
-          <veui-option value="undo" label="撤销"/>
-          <veui-option value="redo" label="重复"/>
-        </veui-option-group>
-      </veui-dropdown>
-    </p>
-    <p>
-      <veui-dropdown
-        ui="link"
+      </p>
+      <p>
+        <span class="veui-font-level-2d">小样式：ui="small"</span>
+      </p>
+      <p>
+        <veui-dropdown
+        ui="small"
         label="操作"
-        disabled
-        :options="options"></veui-dropdown>
-    </p>
+        :options="options">
+          <template slot="option-label" slot-scope="{ label }">
+            👉 {{ label }}
+          </template>
+        </veui-dropdown>
+      </p>
+      <p>
+        <veui-dropdown
+          label="操作操作操作操作操作操作"
+          disabled
+          :options="options"></veui-dropdown>
+      </p>
+    </section>
+    <section>
+      <p>
+        <span class="veui-font-level-1b">文字样式：</span>
+        <span class="veui-font-level-2d">类型ui="link"</span>
+      </p>
+      <p>
+        <veui-dropdown
+          ui="link"
+          label="操作">
+          <veui-option-group label="提交">
+            <veui-option @click="log('save')" label="保存"/>
+            <veui-option value="publish" label="发布"/>
+          </veui-option-group>
+          <veui-option-group label="操作">
+            <veui-option value="undo" label="撤销"/>
+            <veui-option value="redo" label="重复"/>
+          </veui-option-group>
+        </veui-dropdown>
+      </p>
+      <p>
+        <veui-dropdown
+          ui="link"
+          label="操作"
+          disabled
+          :options="options"></veui-dropdown>
+      </p>
+    </section>
   </article>
 </template>
 
