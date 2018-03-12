b69b4e412876b095cbc8fc4c13aa4316974dd082
eyunhua <eyunhua@baidu.com>
2018-03-12 16:35:02 +0800

    新增checkButtonGroup组件样式

--- demo/components/CheckButtonGroup.vue
+++ demo/components/CheckButtonGroup.vue
@@ -3,16 +3,103 @@
     <h1><code>&lt;veui-check-button-group&gt;</code></h1>
     <section>
       <veui-checkbox v-model="disabled">禁用</veui-checkbox>
+      <p>
+        <span class="veui-font-level-1b">与buttonGroup样式一致</span>
+      </p>
     </section>
     <section>
+      <p>
+        <span class="veui-font-level-1b">普通按钮：</span>
+        <span class="veui-font-level-2d">不传ui</span>
+      </p>
+      <p>
+        <span class="veui-font-level-2d">大尺寸：ui="large"</span>
+      </p>
+      <p>
+        <veui-check-button-group :items="data1" ui="large" v-model="picked1" :disabled="disabled">
+          <template slot-scope="props">{{ props.label }}</template>
+        </veui-check-button-group>
+      </p>
+      <p>
+        <span class="veui-font-level-2d">默认尺寸：不传ui</span>
+      </p>
       <p>
         <veui-check-button-group :items="data1" v-model="picked1" :disabled="disabled">
           <template slot-scope="props">{{ props.label }}</template>
         </veui-check-button-group>
       </p>
+      <p>
+        <span class="veui-font-level-2d">小尺寸：ui="small"</span>
+      </p>
+      <p>
+        <veui-check-button-group :items="data1" ui="small" v-model="picked1" :disabled="disabled">
+          <template slot-scope="props">{{ props.label }}</template>
+        </veui-check-button-group>
+      </p>
       <p>value: {{ picked1 }}</p>
+      <p>
+        <veui-check-button-group :items="data3" ui="micro" v-model="picked3" :disabled="disabled">
+          <template slot-scope="props">
+            <veui-icon :name="props.icon" />
+          </template>
+        </veui-check-button-group>
+      </p>
+      <p>value: {{ picked3 }}</p>
     </section>
     <section>
+      <p>
+        <span class="veui-font-level-1b">强调（蓝色）：</span>
+        <span class="veui-font-level-2d">ui="primary"</span>
+      </p>
+      <p>
+        <span class="veui-font-level-2d">大尺寸：ui="large"</span>
+      </p>
+      <p>
+         <veui-check-button-group :items="data1" ui="primary large" v-model="picked1" :disabled="disabled">
+          <template slot-scope="props">{{ props.label }}</template>
+        </veui-check-button-group>
+      </p>
+      <p>
+        <span class="veui-font-level-2d">默认尺寸：不传ui</span>
+      </p>
+      <p>
+        <veui-check-button-group :items="data1" ui="primary" v-model="picked1" :disabled="disabled">
+          <template slot-scope="props">{{ props.label }}</template>
+        </veui-check-button-group>
+      </p>
+      <p>
+        <span class="veui-font-level-2d">小尺寸：ui="small"</span>
+      </p>
+      <p>
+        <veui-check-button-group :items="data1" ui="primary small" v-model="picked1" :disabled="disabled">
+          <template slot-scope="props">{{ props.label }}</template>
+        </veui-check-button-group>
+      </p>
+    </section>
+    <section>
+      <p>
+        <span class="veui-font-level-1b">辅助按钮：</span>
+        <span class="veui-font-level-2d">ui="alt"</span>
+      </p>
+      <p>
+        <span class="veui-font-level-2d">大尺寸：ui="large"</span>
+      </p>
+      <p>
+        <veui-check-button-group :items="data2" ui="large alt" v-model="picked2" :disabled="disabled">
+          <template slot-scope="props">{{ props.label }}</template>
+        </veui-check-button-group>
+      </p>
+      <p>
+        <span class="veui-font-level-2d">默认尺寸：不传ui</span>
+      </p>
+      <p>
+        <veui-check-button-group :items="data2" ui="alt" v-model="picked2" :disabled="disabled">
+          <template slot-scope="props">{{ props.label }}</template>
+        </veui-check-button-group>
+      </p>
+      <p>
+        <span class="veui-font-level-2d">小尺寸：ui="small"</span>
+      </p>
       <p>
         <veui-check-button-group :items="data2" ui="small alt" v-model="picked2" :disabled="disabled">
           <template slot-scope="props">{{ props.label }}</template>
@@ -22,13 +109,14 @@
     </section>
     <section>
       <p>
-        <veui-check-button-group :items="data3" ui="micro" v-model="picked3" :disabled="disabled">
-          <template slot-scope="props">
-            <veui-icon :name="props.icon" />
-          </template>
+        <span class="veui-font-level-1b">间隙排列按钮：</span>
+        <span class="veui-font-level-2d">ui="interval"(可与大小和类型ui样式共同使用)</span>
+      </p>
+      <p>
+        <veui-check-button-group :items="data2" ui="interval small" v-model="picked2" :disabled="disabled">
+          <template slot-scope="props">{{ props.label }}</template>
         </veui-check-button-group>
       </p>
-      <p>value: {{ picked3 }}</p>
     </section>
   </article>
 </template>
