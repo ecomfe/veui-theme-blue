f7e61cb924fb263a62adc04dbde363769901bd55
eyunhua <eyunhua@baidu.com>
2018-10-30 17:08:12 +0800

    修改ButtonGroup组件禁用状态时无边框问题

--- src/components/ButtonGroup.js
+++ src/components/ButtonGroup.js
@@ -3,11 +3,7 @@ import config from 'veui/managers/config';
 config.defaults({
     ui: {
         style: {
-            values: ['alt', 'primary'],
-            inherit: true
-        },
-        size: {
-            values: ['large', 'small', 'tiny', 'micro'],
+            values: ['interval'],
             inherit: true
         }
     }
