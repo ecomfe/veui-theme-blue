97dad5eb0ed9b5c305edb4705f67f71a8385ea7b
lixiaoyang07 <446592589@qq.com>
2020-07-24 14:34:58 +0800

    feat: update numberInput component style(#86)

--- src/components/NumberInput.js
+++ src/components/NumberInput.js
@@ -29,7 +29,7 @@ config.defaults(
             }
         },
         parts: {
-            spinner: 'basic'
+            spinner: 'normal'
         },
         icons: {
             increase: ({style}) => ICON_MAP[style].increase,
