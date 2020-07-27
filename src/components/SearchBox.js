cae61c3330a1a6ff5804309f14a92c866ed161c6
eyunhua <876482803@qq.com>
2020-07-27 14:30:24 +0800

    build: deploy setting (#108) (#169)

---
+++ src/components/SearchBox.js
@@ -0,0 +1,26 @@
+import '../icons/search';
+import config from 'veui/managers/config';
+
+
+config.defaults(
+    {
+        icons: {
+            search: 'search'
+        },
+        parts: {
+            button: 'primary',
+            search: 'icon'
+        },
+        ui: {
+            style: {
+                values: ['normal', 'strong'],
+                inherit: true
+            },
+            size: {
+                values: ['xs', 's', 'm', 'l'],
+                inherit: true
+            }
+        }
+    },
+    'searchbox'
+);
