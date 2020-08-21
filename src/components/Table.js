2d1d639dbf3a86ec6f1bc03d5aae74a2f2ce19b1
eyunhua <876482803@qq.com>
2020-08-21 17:31:58 +0800

    fix(table): add nodata style (#97)

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,18 +1,17 @@
-import '../icons/chevron-double-down';
-import '../icons/chevron-down';
-import '../icons/arrow-down';
+import 'veui-theme-blue-icons/fast-down';
+import 'veui-theme-blue-icons/plain-down';
 import 'veui-theme-blue-icons/sort';
 import config from 'veui/managers/config';
 
 config.defaults(
     {
         icons: {
-            expand: 'chevron-down',
-            collapse: 'chevron-down',
-            expandAll: 'chevron-double-down',
-            collapseAll: 'chevron-double-down',
-            asc: 'arrow-up',
-            desc: 'arrow-down',
+            expand: 'plain-down',
+            collapse: 'plain-down',
+            expandAll: 'fast-down',
+            collapseAll: 'fast-down',
+            asc: 'sort',
+            desc: 'sort',
             sort: 'sort'
         },
         ui: {
