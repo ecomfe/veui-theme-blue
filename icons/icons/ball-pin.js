28e99e6dcf003e2d4a5689f242d737624e75071b
eyunhua <876482803@qq.com>
2022-03-04 19:18:49 +0800

    feat(Icon): update some icons

---
+++ icons/icons/ball-pin.js
@@ -0,0 +1,13 @@
+import Icon from 'veui/components/Icon'
+
+Icon.register({
+  'ball-pin': {
+    paths: [
+      {
+        d: 'M24 9.33a8 8 0 00-16 0 8.01 8.01 0 006.67 7.9v12.1a1.33 1.33 0 002.66 0v-12.1A8.01 8.01 0 0024 9.32zm-8 5.34A5.33 5.33 0 1116 4a5.33 5.33 0 010 10.67z'
+      }
+    ],
+    width: '32',
+    height: '32'
+  }
+})
