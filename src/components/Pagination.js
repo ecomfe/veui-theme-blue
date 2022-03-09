26419865ca31a836114eab2f3b70b4f7e5d5c682
fuchunhui <fuchuhu7@gmail.com>
2022-03-09 21:56:16 +0800

    feat(pagination): update & optimize code.

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,8 +1,9 @@
-import '../icons/chevron-left';
-import '../icons/chevron-right';
-import '../icons/chevron-double-left';
-import '../icons/chevron-double-right';
-import '../icons/ellipsis';
+import 'veui-theme-blue-icons/plain-left';
+import 'veui-theme-blue-icons/plain-right';
+import 'veui-theme-blue-icons/fast-left';
+import 'veui-theme-blue-icons/fast-right';
+import 'veui-theme-blue-icons/more-ellipsis';
+
 import ui from 'veui/managers/ui';
 import i18n from 'veui/managers/i18n';
 
@@ -17,30 +18,28 @@ i18n.register(
         next: '下一页',
         pageLabel: '前往第{page}页',
         current: '第{page}页，当前页',
-        gotoPage: '去第{page}页',
+        gotoPage: '跳至{page}页',
         go: '确定'
     },
     {
         ns: 'pagination'
     }
 );
+
 ui.defaults(
     {
         icons: {
-            prev: 'chevron-left',
-            next: 'chevron-right',
-            backward: 'chevron-double-left',
-            forward: 'chevron-double-right',
-            more: 'ellipsis'
+            prev: 'plain-left',
+            next: 'plain-right',
+            backward: 'fast-left',
+            forward: 'fast-right',
+            more: 'more-ellipsis'
         },
         ui: {
             size: {
                 values: ['xs', 's', 'm'],
                 inherit: true,
                 default: 's'
-            },
-            style: {
-                values: ['normal']
             }
         }
     },
