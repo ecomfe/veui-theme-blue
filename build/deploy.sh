5d35591f914d9d2b148b2b27589a86390458d6ae
eyunhua <876482803@qq.com>
2022-07-15 16:34:40 +0800

    chore(pnpm): upgrade npm to pnpm. #108

--- build/deploy.sh
+++ build/deploy.sh
@@ -1,4 +1,4 @@
-npm run build
-echo '  \033[32m npm run build. \033[0m \n'
+pnpm run build
+echo '  \033[32m pnpm run build. \033[0m \n'
 
 # sed -i "" 's/static\/img/\..\/..\/static\/img/g' `grep static/img -rl components`
